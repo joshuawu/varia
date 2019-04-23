c0ddf62899d4a0a63690e8f4d5e3f483639b6f90
Joshua Tsubaki Wu <wujoshuawu@gmail.com>
2019-04-23 21:34:33 +0900

    Add helpful state change methods for vanilla JS objects (non-strict version of Immutable.js).

--- lib/state.ts
+++ lib/state.ts
@@ -7,3 +7,18 @@ export const assoc = <T extends object, K extends keyof T>(
   result[key] = value
   return result
 }
+
+export const modify = <T extends object, K extends keyof T>(
+  object: T,
+  key: K,
+  fn: (value: T[K]) => T[K],
+): T => {
+  const result = Object.assign({}, object)
+  result[key] = fn(result[key])
+  return result
+}
+
+export const merge = <T extends object>(
+  object: T,
+  ...others: Partial<T>[]
+): T => Object.assign({}, object, ...others)
